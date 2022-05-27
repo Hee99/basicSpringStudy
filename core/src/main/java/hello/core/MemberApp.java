d55f90bba143b3bb5d70dcebfb849ccb1ea73af4
119tp <119tp@DESKTOP-BMPR74F>
2022-05-27 15:51:56 +0900

    컨테이너에 등록된 모든 빈 조회

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,16 +1,25 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.AppConfig;
 
 public class MemberApp {
 	
 	public static void main(String[] args) {
-		AppConfig appConfig = new AppConfig();
-		MemberService memberService = appConfig.memberService();
+//		AppConfig appConfig = new AppConfig();
+//		MemberService memberService = appConfig.memberService();
+		
+		//Spring 사용하도록 전환하기
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		//AppConfig가 가지고있는 구성정보를 가지고 @Bean어노테이션이 붙은 객체들을 모두 spring bean에 등록하여 관리해줌
+		MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+		//getBean(메소드명, 반환타입)
+		
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberService.join(member);
 		
--- core/src/main/java/hello/core/order/AppConfig.java
+++ core/src/main/java/hello/core/order/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core.order;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
@@ -7,21 +10,28 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
+//구성정보 작성하는 클래스에 지정하는 어노테이션
+@Configuration
 public class AppConfig {
 
-	private MemberRepository memberRepository() {
+	//spring컨테이너에 등록하는 어노테이션
+	@Bean
+	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 	
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		//return new FixDiscountPolicy();
 		return new RateDiscountPolicy();
 	}
-	
+
+	@Bean
 	public MemberService memberService() {
 		return new MemberServiceImpl(memberRepository());
 	}
-	
+
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
