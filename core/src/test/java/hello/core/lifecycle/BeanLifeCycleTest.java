99f86b24594ce5862a3e151aea37c5684ccbfce2
119tp <119tp@DESKTOP-BMPR74F>
2022-06-03 09:49:40 +0900

    빈 생성주기 콜백 - 인터페이스

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,29 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+	
+	@Test
+	public void lifeCycleTest() {
+		//close가능한 ApplicationContext의 하위 클래스 사용
+		ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCyCleConfig.class);
+		NetworkClient client = ac.getBean(NetworkClient.class);
+		ac.close();
+	}
+	
+	@Configuration
+	static class LifeCyCleConfig {
+		@Bean
+		public NetworkClient networkClient() {
+			NetworkClient networkClient = new NetworkClient();
+			networkClient.setUrl("http://hello-spring.dev");
+			return networkClient;
+		}
+	}
+
+}
---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,45 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean{
+	
+	private String url;
+	
+	public NetworkClient() {
+		System.out.println("생성자 호출, url = " + url);
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	//서비스 시작시 호출
+	public void connect() {
+		System.out.println("connect : " + url);
+	}
+	
+	public void call(String message) {
+		System.out.println("call: " + url + " message = " + message);
+	}
+	
+	//서비스 종료시 호출
+	public void disconnect() {
+		System.out.println("close " + url);
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception { //의존관계 주입 끝나고 호출되는 메소드
+		System.out.println("NetworkClient.afterPropertiesSet");
+		connect();
+		call("초기화 연결 메시지");
+	}
+
+	@Override
+	public void destroy() throws Exception { //빈 종료 직전 호출되는 메소드
+		System.out.println("NetworkClient.destroy");
+		disconnect();
+	}
+
+}
